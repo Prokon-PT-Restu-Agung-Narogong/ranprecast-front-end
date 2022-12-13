5cc31f303856570d3c22da8a1093a4b38d30a293
- <74578072+darmawan06@users.noreply.github.com>
2022-12-13 10:12:54 +0700

    fixed api visitors

--- src/scripts/utils/getDataUser.js
+++ src/scripts/utils/getDataUser.js
@@ -2,13 +2,14 @@ const getDataUser = {
 
 	async getAll(){
 		this.data = {};
-		await $.getJSON("http://ip-api.com/json",async (data)=> {
-        	this.data = await {
-        		ip_address : data.query,
-        		country : data.country,
-        		regionName : data.regionName,
+		let ip = await $.getJSON('https://api.ipify.org/?format=json')
+		await $.getJSON(`https://ipapi.co/${ip.ip}/json/`,async (data)=> {
+        	this.data = {
+        		ip_address : data.ip,
+        		country : data.country_name,
+        		regionName : data.region,
         		city : data.city,
-        		isp : data.isp,
+        		isp : data.org,
         		browser : navigator.appCodeName,
         		date : new Date().toString()
         	};
--- src/scripts/views/pages/feedback.js
+++ src/scripts/views/pages/feedback.js
@@ -8,7 +8,7 @@ const aboutUs = {
 			${await btnContact.init()}
 			<section class="w-full h-auto flex flex-col py-[100px]  bg-red-600">
 				<div class="w-full h-full flex">
-					<form id="testimoniForm" class="w-full w-lg-[400px] h-auto bg-white m-auto flex flex-col px-[40px]">
+					<form id="testimoniForm" class="w-full md:w-[400px] h-auto bg-white m-auto flex flex-col px-[40px]">
 						<img src="${this.dataTentang.logo}" class="w-[60px] h-[60px] mx-auto mt-[20px]">
 						<h3 class="w-full text-center mt-[20px] font-primary text-[17px] font-[600] uppercase">Feedback</h3>
 						<h4 class="text-black font-primary text-center mt-[40px] text-[13px]">Bagaimana penilaianmu terhadap Ranprecast?</h4>
